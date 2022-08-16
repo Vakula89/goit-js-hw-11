49c5c0b58c972f838d813cb4bcc2ec5319eddf22
Vakula89 <vasyl.yento@gmail.com>
2022-08-16 20:54:18 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const btnStart = document.querySelector("[data-start]")
+const btnStop = document.querySelector("[data-stop]")
+const body = document.querySelector("body")
+let setTimeoutHandler = 0
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+}
+
+function bgColorChanger(){
+    body.style.backgroundColor = getRandomHexColor();
+    if(btnStart.getAttribute("disabled")){
+        setTimeoutHandler = setTimeout(() => {
+            bgColorChanger()
+        },1000)
+    }
+}
+
+btnStart.addEventListener("click", (ev) => {
+    if(!ev.target.getAttribute("disabled")){
+        ev.target.setAttribute("disabled","disabled")
+    }
+    btnStop.removeAttribute("disabled")
+    bgColorChanger()
+})
+
+btnStop.addEventListener("click", (ev) => {
+    if(!btnStart.getAttribute("disabled")){
+        return 
+    }
+    if(!ev.target.getAttribute("disabled")){
+        ev.target.setAttribute("disabled","disabled")
+    }
+    btnStart.removeAttribute("disabled")
+    if(setTimeoutHandler){
+        clearTimeout(setTimeoutHandler)
+    }
+    setTimeoutHandler = 0
+})
