e2693ebbb23cd3a319920835dc20457560295d36
Vakula89 <vasyl.yento@gmail.com>
2022-08-23 16:39:23 +0300

    Final

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Notiflix from 'notiflix'
 import "notiflix/dist/notiflix-3.2.5.min.css"
 import debounce from "lodash.debounce"
 
-const DEBOUNCE_DELAY = 400;
+const DEBOUNCE_DELAY = 300;
 const searchBox = document.querySelector("#search-box")
 const countryList = document.querySelector(".country-list")
 const countryInfo = document.querySelector(".country-info")
